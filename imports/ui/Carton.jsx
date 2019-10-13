711506fd16ff3a6884299b3648ad6db59aac1065
Laura <l.maya10@uniandes.edu.co>
2019-10-13 12:18:08 -0500

    Funciona sin seguridad

--- imports/ui/Carton.jsx
+++ imports/ui/Carton.jsx
@@ -6,39 +6,6 @@ import "./Carton.css";
 
 const Carton = props => {
 
-	let f = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-	let o = [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-	let p = [31,32,33,34,35,36,37,38,39,40,41,42,43,44,45];
-	let r = [46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
-	let e = [61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
-	
-	let n;
-	let a=[];
-	for (n=1; n<=5; ++n)
-	{
-  		let i = Math.floor((Math.random() * (15-n)) + 1);
-  		a.push(f[i]);
-  		f[i] = f[15-n];
-
-  		i = Math.floor((Math.random() * (15-n)) + 1);
-  		a.push(o[i]);
-  		o[i] = o[15-n];
-
-  		i = Math.floor((Math.random() * (15-n)) + 1);
-  		a.push(p[i]);
-  		p[i] = p[15-n];
-
-  		i = Math.floor((Math.random() * (15-n)) + 1);
-  		a.push(r[i]);
-  		r[i] = r[15-n];
-
-  		i = Math.floor((Math.random() * (15-n)) + 1);
-  		a.push(e[i]);
-  		e[i] = e[15-n];
-	}
-
-
-
 	return (
 		(props.user != null && props.user.username != "admin") ? 
 		(
@@ -53,8 +20,8 @@ const Carton = props => {
 			</div>
 			<br/>
 			<div className = "row">
-				{a.map((n,i)  => (
-					<NumeroCarton valor={a[i]} key={i}></NumeroCarton>
+				{props.numeros.map((n,i)  => (
+					<NumeroCarton valor={props.numeros[i]} key={i}></NumeroCarton>
 					))
 				}
 
@@ -71,7 +38,8 @@ const Carton = props => {
 
 
 Carton.propTypes = {
-	user : PropTypes.object
+	user : PropTypes.object,
+	numeros : PropTypes.arrayOf(PropTypes.number).isRequired
 };
 
 export default Carton;
--- imports/ui/Juego.jsx
+++ imports/ui/Juego.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Tablero from './Tablero.jsx';
 import Carton from './Carton.jsx';
 import PropTypes from 'prop-types';
@@ -6,7 +6,44 @@ import "./Juego.css";
 
 const Juego = props => {
 
+	const[a, setA] = useState([]);
+
+	useEffect(()=> {
+
+		let f = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+		let o = [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+		let p = [31,32,33,34,35,36,37,38,39,40,41,42,43,44,45];
+		let r = [46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
+		let e = [61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
+	
+		let n;
+		for (n=1; n<=5; ++n)
+		{
+	  		let i = Math.floor((Math.random() * (15-n)) + 1);
+	  		a.push(f[i]);
+	  		f[i] = f[15-n];
+
+	  		i = Math.floor((Math.random() * (15-n)) + 1);
+	  		a.push(o[i]);
+	  		o[i] = o[15-n];
+
+	  		i = Math.floor((Math.random() * (15-n)) + 1);
+	  		a.push(p[i]);
+	  		p[i] = p[15-n];
+
+	  		i = Math.floor((Math.random() * (15-n)) + 1);
+	  		a.push(r[i]);
+	  		r[i] = r[15-n];
+
+	  		i = Math.floor((Math.random() * (15-n)) + 1);
+	  		a.push(e[i]);
+	  		e[i] = e[15-n];
+		}
+
+	},[])
+	
 	return (
+
 		<div>
 			<div className ="container-fluid contenedor">
 				<div className = "row tableroPrincipal">
@@ -14,7 +51,7 @@ const Juego = props => {
 					
 				</div>
 				<div className ="row">
-					<Carton user = {props.user}/>
+					<Carton numeros = {a} user = {props.user}/>
 				</div>			
 			</div>
 		</div>
